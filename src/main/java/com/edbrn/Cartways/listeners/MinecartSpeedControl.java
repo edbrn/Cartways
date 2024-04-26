0fa3765b151343d99b22965b3f39a45f5a98c823
Jordan Brown <hello@edbrn.com>
2024-04-26 22:11:21 +0100

    Get trains stopping at stations periodically

--- src/main/java/com/edbrn/Cartways/listeners/MinecartSpeedControl.java
+++ src/main/java/com/edbrn/Cartways/listeners/MinecartSpeedControl.java
@@ -2,6 +2,9 @@ package com.edbrn.Cartways.listeners;
 
 import java.util.logging.Logger;
 
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.block.sign.Side;
 import org.bukkit.entity.Minecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,13 +21,40 @@ public class MinecartSpeedControl implements Listener {
         this.logger = logger;
     }
 
+    private boolean isStationBlockNear(Block block) {
+        if (block.getType().name().equals("OAK_WALL_SIGN")) {
+            Sign sign = (Sign) block.getState();
+            if (sign.getSide(Side.FRONT).getLine(0).equals("[STATION]")) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     @EventHandler
     public void onVehicleMove(VehicleMoveEvent event) {
         if (event.getVehicle().getName().equals("Minecart") && event.getVehicle().getTicksLived() > 20) {
             Minecart minecart = (Minecart) event.getVehicle();
             MinecartState minecartState = SpeedManagedMinecarts.getShared().getMinecartState(minecart);
-            Vector direction = minecartState.getInstance().getFacing().getDirection();
-            minecartState.getInstance().setVelocity(new Vector(direction.getX(), direction.getY(), direction.getZ()));
+
+            if (minecartState.getIsAtStation()) {
+                return;
+            }
+
+            Block nextBlock = event.getFrom().getBlock();
+            if (
+                this.isStationBlockNear(nextBlock.getRelative(1, 2, 0))
+                || this.isStationBlockNear(nextBlock.getRelative(-1, 2, 0))
+                || this.isStationBlockNear(nextBlock.getRelative(0, 2, 1))
+                || this.isStationBlockNear(nextBlock.getRelative(0, 2, -1))
+            ) {
+                minecartState.setReachedStation();
+                minecart.setVelocity(new Vector(0, 0, 0));
+            } else {
+                Vector direction = minecartState.getInstance().getFacing().getDirection();
+                minecartState.getInstance().setVelocity(new Vector(direction.getX(), direction.getY(), direction.getZ()));
+            }
         }
     }
 }
--- src/main/java/com/edbrn/Cartways/state/MinecartState.java
+++ src/main/java/com/edbrn/Cartways/state/MinecartState.java
@@ -1,9 +1,15 @@
 package com.edbrn.Cartways.state;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Minecart;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+
+import com.edbrn.Cartways.App;
 
 public class MinecartState {
     private Minecart instance;
+    private boolean isAtStation = false;
 
     public MinecartState(Minecart minecart) {
         this.instance = minecart;
@@ -11,5 +17,30 @@ public class MinecartState {
 
     public Minecart getInstance() {
         return this.instance;
+    }
+
+    public boolean getIsAtStation() {
+        return isAtStation;
+    }
+
+    public void setLeftStation() {
+        isAtStation = false;
+    }
+
+    public void setReachedStation() {
+        isAtStation = true;
+        MinecartState self = this;
+        Bukkit.getScheduler().scheduleSyncDelayedTask(JavaPlugin.getPlugin(App.class), new Runnable() {
+            public void run() {
+                Vector direction = self.instance.getFacing().getDirection();
+                self.instance.setVelocity(new Vector(direction.getX(), direction.getY(), direction.getZ()));
+                
+                Bukkit.getScheduler().scheduleSyncDelayedTask(JavaPlugin.getPlugin(App.class), new Runnable() {
+                    public void run() {
+                        self.setLeftStation();
+                    }
+                }, 20);
+            }
+        }, 90);
     }
 }
